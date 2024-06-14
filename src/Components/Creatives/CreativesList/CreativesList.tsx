e866a444515e64080b57272faf3107bf47852909
Yevhen <camelcaseman@gmail.com>
2024-06-14 11:49:49 +0200

    Creatives 70%

--- src/Components/Creatives/CreativesList/CreativesList.tsx
+++ src/Components/Creatives/CreativesList/CreativesList.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { useFetchAllCreativesQuery } from "@/store/wordpress/wpRestApi";
+import { useFetchAllCreativesByDataQuery } from "@/store/wordpress/wpRestApi";
 import CreativesListItem from "../CreativesListItem";
 import { CreativeDataType } from "@/types/components/Creative";
 
 const CreativesList = () => {
 
-    const { data: creatives = [], isLoading, isError, error } = useFetchAllCreativesQuery({ per_page: 5 });
+    const { data: creatives = [], isLoading, isError, error } = useFetchAllCreativesByDataQuery({ per_page: 10, offset: 0 });
+
+    const renderListItems = () => {
+        return Boolean(creatives.length) && creatives.map((creative: CreativeDataType) => (
+            <CreativesListItem key={creative.id} creative={creative} hasLiked={false} />
+        ))
+    }
 
     return (
         <div className="container container_creatives">
             <div className="creatives-list-grid">
-                {Boolean(creatives.length) && creatives.map((creative: CreativeDataType) => (
-                    <CreativesListItem creative={creative} hasLiked={false} />
-                ))}
+                {renderListItems()}
             </div>
         </div>
     )
