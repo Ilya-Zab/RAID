e9e0f51d1dd1f2994759a250b7f469dc61920098
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-06-21 21:08:49 +0300

    main page v4

--- src/Components/Layouts/Popular/Popular.tsx
+++ src/Components/Layouts/Popular/Popular.tsx
@@ -1,10 +1,52 @@
 import * as React from 'react';
 import styles from './styles.module.scss';
 import {Box} from '@mui/material';
+import Image from 'next/image';
+import {useEffect, useRef, useState} from "react";
 
 const Popular = () => {
+
+    const beforeRef = useRef(null);
+    const [topDistance, setTopDistance] = useState(0);
+    const [computedTop, setComputedTop] = useState('');
+    const headerHeight = 0;
+    const coefficient = 0.2;
+
+    useEffect(() => {
+        const isTablet = window.innerWidth >= 1024;
+        const isMobile = window.innerWidth >= 768;
+        const defaultTop = isMobile ? (isTablet ? -651 : -401) : -351;
+        const handleScroll = () => {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+            let distanceFromHeader = Math.max(scrollTop - headerHeight, 0);
+            distanceFromHeader *= coefficient;
+
+            setTopDistance(distanceFromHeader);
+            setComputedTop(`${defaultTop + distanceFromHeader}px`);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <Box className={styles.popular}>
+            <Box className={styles.popular__img__wrapper} style={{ top: computedTop}}>
+                <Image
+                    ref={beforeRef}
+                    style={{ top: computedTop}}
+                    src='/images/vlad.png'
+                    alt='vlad'
+                    width={635}
+                    height={804}
+                    className={styles.popular__img}
+                />
+            </Box>
             <Box className={styles.container}>
                 <Box className={styles.popular__title__wrapper}>
                     <h2 className={`text-gradient ${styles.popular__title}`}>
@@ -15,7 +57,7 @@ const Popular = () => {
                     <h3 className={`text-gradient ${styles.popular__title} ${styles.popular__title_sub}`}>
                         Popular
                     </h3>
-                    <Box>
+                    <Box className={styles.popular__list}>
 
                     </Box>
                 </Box>
@@ -23,7 +65,7 @@ const Popular = () => {
                     <h3 className={`${styles.popular__title} ${styles.popular__title_sub}`}>
                         Latest
                     </h3>
-                    <Box>
+                    <Box className={styles.popular__list}>
 
                     </Box>
                 </Box>
--- src/Components/Layouts/Second/Second.tsx
+++ src/Components/Layouts/Second/Second.tsx
@@ -1,40 +1,52 @@
 import React, { useRef, useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import { Box } from '@mui/material';
+import Image from 'next/image';
 
 const Second = () => {
     const beforeRef = useRef(null);
-    const [isFixed, setIsFixed] = useState(false);
+    const [topDistance, setTopDistance] = useState(0);
+    const [computedTop, setComputedTop] = useState('');
+    const headerHeight = 0;
+    const coefficient = 0.2;
+
+
+
 
     useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                if (entry.intersectionRatio >= 1) {
-                    setIsFixed(true);
-                } else {
-                    setIsFixed(false);
-                }
-            },
-            {
-                root: null,
-                rootMargin: ' 0px',
-                threshold: 1
-            }
-        );
+        const isMobile = window.innerWidth >= 768;
+        const defaultTop = isMobile ? -410 : -300;
+        const handleScroll = () => {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (beforeRef.current) {
-            observer.observe(beforeRef.current);
-        }
+            let distanceFromHeader = Math.max(scrollTop - headerHeight, 0);
+            distanceFromHeader *= coefficient;
+
+            setTopDistance(distanceFromHeader);
+            setComputedTop(`${defaultTop + distanceFromHeader}px`);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
 
         return () => {
-            if (beforeRef.current) {
-                observer.unobserve(beforeRef.current);
-            }
+            window.removeEventListener('scroll', handleScroll);
         };
     }, []);
 
     return (
-        <Box className={`${styles.second} ${isFixed ? styles.fixed : ''}`} ref={beforeRef}>
+        <Box className={`${styles.second}`}>
+            <Box>
+                <Image
+                    ref={beforeRef}
+                    style={{ top: computedTop}}
+                    src='/images/brakus_the_Shifter.png'
+                    alt='wolf'
+                    width={816}
+                    height={1120}
+                    className={styles.second__img}
+                />
+            </Box>
             <Box className={styles.container}>
                 <Box className={styles.second__title_wrapper}>
                     <h2 className={styles.second__title}>
