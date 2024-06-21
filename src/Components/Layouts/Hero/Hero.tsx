f642ffb9d56982b577038841ccc534a764ac622b
Ilya-Zab <anno.201543@icloud.com>
2024-06-21 11:05:06 +0200

    Fixed some issue to build the project

--- src/Components/Layouts/Hero/Hero.tsx
+++ src/Components/Layouts/Hero/Hero.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import styles from './styles.module.scss';
 import Link from "next/link";
-import {Box} from '@mui/material';
+import { Box } from '@mui/material';
 
-const Hero = () => {
+const Hero = () =>
+{
     return (
         <Box className={styles.container}>
             <Box className={styles.hero__title_wrapper}>
                 <h1 className={styles.hero__title}>
-                    Everyone's <br/>
+                    Everyone&apos;s <br />
                     <span className='text-gradient'>Playing Raid</span>
                     .
-                    <br/>
+                    <br />
                     Start Now and
-                    <br/>
+                    <br />
                     <span className='text-gradient'>win</span>
                     a huge
                     <span className='text-gradient'>prize.</span>
@@ -30,12 +31,12 @@ const Hero = () => {
                     <b>Step 2: </b>
                     enter your&nbsp;
                     <Link href={'/'} className='text-gradient'>
-                          RAID ID
+                        RAID ID
                     </Link>
                 </p>
                 <p className={styles.hero__steps}>
                     <b>Step 3: </b> create or upload your content
-                    <br/>
+                    <br />
                     to take part in the prize draw!
                 </p>
             </Box>
