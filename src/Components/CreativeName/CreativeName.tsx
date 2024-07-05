8749747fdf0b06ebac40153b7c829ea5eb87f867
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2024-07-05 15:24:13 +0300

    v1

---
+++ src/Components/CreativeName/CreativeName.tsx
@@ -0,0 +1,28 @@
+import {Box, Button} from "@mui/material";
+import styles from "./styles.module.scss";
+import Image from "next/image";
+
+const CreativeName = ({nextStep}) =>{
+    return (
+        <Box>
+            <Image
+                src={'./eqeq.png'}
+                alt={""}
+                width={340}
+                height={605}
+                className={styles.img}
+            />
+            <form className={styles.form}>
+                <input type="text" className={styles.input} placeholder={'Enter Your Nickname'}/>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    className={`btn-second btn-second-next`}
+                    onClick={nextStep}>
+                    Next
+                </Button>
+            </form>
+        </Box>
+    )
+}
+export default CreativeName;
