8e883f89a5ca7f1e8ff9089854804f11f5de7acb
Ilya-Zab <anno.201543@icloud.com>
2024-07-10 10:57:11 +0200

    Chaned input behaviour

--- src/Components/CreateVideoTemplate/CreateVideoTemplate.tsx
+++ src/Components/CreateVideoTemplate/CreateVideoTemplate.tsx
@@ -112,7 +112,7 @@ const CreateVideoTemplate: React.FC<CreateVideoTemplateProps> = ({ handleButtonC
         }
     }
 
-    const onSocialClick = async () =>
+    const onInputBtnClick = async () =>
     {
         if (url)
         {
@@ -144,7 +144,7 @@ const CreateVideoTemplate: React.FC<CreateVideoTemplateProps> = ({ handleButtonC
                     />
                     <button
                         type='button'
-                        onClick={onSocialClick}
+                        onClick={onInputBtnClick}
                     >
                         <svg width="25" height="25" viewBox="0 0 25 25" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
