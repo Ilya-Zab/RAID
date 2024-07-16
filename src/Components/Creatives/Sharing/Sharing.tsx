592ce3914d7fea9de23565d0183d519bdc2455b7
Yevhen <camelcaseman@gmail.com>
2024-07-16 12:25:16 +0200

    Sharing

--- src/Components/Creatives/Sharing/Sharing.tsx
+++ src/Components/Creatives/Sharing/Sharing.tsx
@@ -1,23 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import Image from "next/image";
+import { FacebookShareButton, TwitterShareButton } from "react-share";
 
 const Sharing = ({ title, text, creativeId }) => {
+    const [domain, setDomain] = useState('');
+    useEffect(() => {
+        setDomain(window.location.origin);
+    }, []);
 
-    const shareX = (creativeId) => {
-
-    }
-
-    const shareFb = (creativeId) => {
-
-    }
-
-    const shareTt = (creativeId) => {
-
-    }
 
     const shareLink = (creativeId) => {
-
+        const link = `${domain}/preview?creative=${creativeId}`;
+        navigator.clipboard.writeText(link);
+        alert("Link copied to clipboard.")
     }
 
     return (
@@ -31,15 +27,29 @@ const Sharing = ({ title, text, creativeId }) => {
                 </div>
             </div>
             <div className={styles['sharing__buttons']}>
-                <button className={styles['sharing__button']} onClick={() => shareX(creativeId)}>
-                    <Image width={40} height={40} alt="x" src={'/images/sharing-x.svg'} />
-                </button>
-                <button className={styles['sharing__button']} onClick={() => shareFb(creativeId)}>
-                    <Image width={40} height={40} alt="x" src={'/images/sharing-fb.svg'} />
-                </button>
-                <button className={styles['sharing__button']} onClick={() => shareTt(creativeId)}>
+
+                <div className={styles['sharing__button']}>
+                    <TwitterShareButton
+                        url={`${domain}/preview?creative=${creativeId}`}
+                        aria-label="share via twitter"
+                    >
+                        <Image width={40} height={40} alt="x" src={'/images/sharing-x.svg'} />
+                    </TwitterShareButton>
+                </div>
+                <div className={styles['sharing__button']}>
+                    <FacebookShareButton
+                        url={`${domain}/preview?creative=${creativeId}`}
+                        aria-label="share via facebook"
+                    >
+                        <Image width={40} height={40} alt="x" src={'/images/sharing-fb.svg'} />
+                    </FacebookShareButton>
+                </div>
+                {/* <button className={styles['sharing__button']} onClick={() => shareTt(creativeId)}>
+
                     <Image width={40} height={40} alt="x" src={'/images/sharing-tt.svg'} />
-                </button>
+
+                </button> */}
+
                 <button className={styles['sharing__button']} onClick={() => shareLink(creativeId)}>
                     <Image width={40} height={40} alt="x" src={'/images/sharing-share.svg'} />
                 </button>
--- src/types/components/Creative/CreativesListItemProps.ts
+++ src/types/components/Creative/CreativesListItemProps.ts
@@ -4,7 +4,8 @@ import { CreativeDataTypeSchema } from "./CreativeDataType";
 export const CreativesListItemPropsSchema = z.object({
     creative: CreativeDataTypeSchema,
     hasVoted: z.boolean(),
-    onVote: z.any()
+    onVote: z.any(),
+    shared: z.boolean().optional()
 });
 
 export type CreativesListItemProps = z.infer<typeof CreativesListItemPropsSchema>;
