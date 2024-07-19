ccab236766eab72d0aef6eb83e32fe0e2b5c2d5e
Ilya-Zab <anno.201543@icloud.com>
2024-07-19 09:13:04 +0200

    Adde google tag manager script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,21 +18,21 @@ export default function Document()
         <meta name="twitter:image" content="/public/images/sharing-preview.jpg" />
         <meta name="twitter:title" content="wefinallyplayedit" />
         <meta name="twitter:description" content="wefinallyplayedit" />
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script
+          id="google-tag-manager"
+          src="https://www.googletagmanager.com/gtag/js?id=G-R0XBSWWZGF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag-config" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N3ZT2NVW');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R0XBSWWZGF');
           `}
         </Script>
       </Head>
       <body>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3ZT2NVW"
-            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
-        </noscript>
         <Main />
         <NextScript />
       </body>
