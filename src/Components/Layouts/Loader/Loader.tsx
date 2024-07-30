01c0efd1c5d4d405947855edde9ecc80275344b2
Ilya-Zab <anno.201543@icloud.com>
2024-07-30 16:52:49 +0200

    chnaged laoder

--- src/Components/Layouts/Loader/Loader.tsx
+++ src/Components/Layouts/Loader/Loader.tsx
@@ -1,15 +1,32 @@
 
-import { Box, Typography } from "@mui/material";
+import { Box, keyframes, Typography } from "@mui/material";
 import { FC, useEffect, useState } from "react";
 import variable from '@/styles/variables.module.scss';
 import { LoaderProps } from "@/types/components/Layouts/Loader";
 import CircularProgress, {
     CircularProgressProps,
 } from '@mui/material/CircularProgress';
+
+const waiting = keyframes`
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.3;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 export const Loader: FC<LoaderProps> = ({ className, thickness, size, color, progress }) =>
 {
-    const [currentProgress, setCurrentProgress] = useState(0);
-    const [targetProgress, setTargetProgress] = useState(progress);
+    const [currentProgress, setCurrentProgress] = useState<number>(0);
+    const [targetProgress, setTargetProgress] = useState<number>(progress);
+
+    useEffect(() =>
+    {
+        setTargetProgress(progress);
+    }, [progress]);
 
     useEffect(() =>
     {
@@ -36,6 +53,8 @@ export const Loader: FC<LoaderProps> = ({ className, thickness, size, color, pro
         return () => clearInterval(interval);
     }, [targetProgress, currentProgress]);
 
+    const animationStyle = currentProgress === targetProgress ? `${waiting} 2s infinite` : 'none';
+
     return (
         <Box
             sx={{
@@ -54,32 +73,34 @@ export const Loader: FC<LoaderProps> = ({ className, thickness, size, color, pro
             className={className}
         >
             <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-                <CircularProgress
-                    variant="determinate"
-                    size={size}
-                    thickness={thickness}
-                    value={currentProgress}
-                    sx={{
-                        color: color ? color : variable.accent,
-                    }}
-                />
-                <Box
-                    sx={{
-                        top: 0,
-                        left: 0,
-                        bottom: 0,
-                        right: 0,
-                        position: 'absolute',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}
-                >
-                    <Typography
-                        variant="caption"
-                        component="div"
-                        color="white"
-                    >{currentProgress}%</Typography>
+                <Box sx={{ animation: animationStyle }}>
+                    <CircularProgress
+                        variant="determinate"
+                        size={size}
+                        thickness={thickness}
+                        value={currentProgress}
+                        sx={{
+                            color: color ? color : variable.accent,
+                        }}
+                    />
+                    <Box
+                        sx={{
+                            top: 0,
+                            left: 0,
+                            bottom: 0,
+                            right: 0,
+                            position: 'absolute',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Typography
+                            variant="caption"
+                            component="div"
+                            color="white"
+                        >{currentProgress}%</Typography>
+                    </Box>
                 </Box>
             </Box>
             <Box sx={{ padding: '30px', fontSize: '12px', textAlign: 'center' }}>
@@ -87,6 +108,6 @@ export const Loader: FC<LoaderProps> = ({ className, thickness, size, color, pro
                 <a className="text-gradient text-gradient_alt" href={"mailto:support@wefinallyplayedit.com"}> support@wefinallyplayedit.com </a><br />
                 <span>to our team will help to publish it!</span>
             </Box>
-        </Box>
+        </Box >
     )
 }
